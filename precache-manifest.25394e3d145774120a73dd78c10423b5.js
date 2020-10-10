b49394fbd10d974ca0d94394c6906ad9290be79d
archerjessie <66236559+archerjessie@users.noreply.github.com>
2020-10-10 21:06:01 +1100

    Updates

---
+++ precache-manifest.25394e3d145774120a73dd78c10423b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8b5c3fced2161e418a3a1e463f2724",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "0bd719ec3357b70c5757",
+    "url": "/weather-app/static/css/main.29847642.chunk.css"
+  },
+  {
+    "revision": "05425f85ea7f1ae0ea20",
+    "url": "/weather-app/static/js/2.75606a5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.75606a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd719ec3357b70c5757",
+    "url": "/weather-app/static/js/main.745bd3a3.chunk.js"
+  },
+  {
+    "revision": "90b6511dd9f7afcab50f",
+    "url": "/weather-app/static/js/runtime-main.5d4ec171.js"
+  },
+  {
+    "revision": "0ff5d82a5cac8f9ab2eea8211f7ada59",
+    "url": "/weather-app/static/media/Brisbane.0ff5d82a.jpg"
+  },
+  {
+    "revision": "47decd0e69d8904a84d5240fb612723d",
+    "url": "/weather-app/static/media/Perth.47decd0e.jpg"
+  },
+  {
+    "revision": "265ddec0c3b7c356937dfe49551c7d22",
+    "url": "/weather-app/static/media/Sydney.265ddec0.jpg"
+  }
+]);
